3603d4d2ee97c97969a0bf42a04728a3cab10cf4
Jonathan <48375298+JonathanDowdell@users.noreply.github.com>
2023-12-01 21:17:19 -0800

    📦 chore(mongoadapter.go): add MongoAdapter interface and implementation
    📦 chore(go.mod): add required dependencies for the project

---
+++ adapter/mongoadapter.go
@@ -0,0 +1,47 @@
+package adapter
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoAdapter interface {
+	InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error)
+	UpdateOne(ctx context.Context, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error)
+	DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error)
+	FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult
+	Find(ctx context.Context, filter interface{}, opts ...*options.FindOptions) (cur *mongo.Cursor, err error)
+}
+
+type MongoAdapterImpl struct {
+	collection *mongo.Collection
+}
+
+func NewMongoAdapter(collection *mongo.Collection) *MongoAdapterImpl {
+	return &MongoAdapterImpl{collection: collection}
+}
+
+func (m *MongoAdapterImpl) InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
+	return m.collection.InsertOne(ctx, document, opts...)
+}
+
+func (m *MongoAdapterImpl) UpdateOne(ctx context.Context, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
+	return m.collection.UpdateOne(ctx, filter, update, opts...)
+}
+
+func (m *MongoAdapterImpl) DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
+	return m.collection.DeleteOne(ctx, filter, opts...)
+}
+
+func (m *MongoAdapterImpl) FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult {
+	return m.collection.FindOne(ctx, filter, opts...)
+}
+
+func (m *MongoAdapterImpl) Find(ctx context.Context, filter interface{}, opts ...*options.FindOptions) (cur *mongo.Cursor, err error) {
+	return m.collection.Find(ctx, filter, opts...)
+}
+
+// Ensure that MongoAdapterImpl implements MongoAdapter
+var _ MongoAdapter = (*MongoAdapterImpl)(nil)
