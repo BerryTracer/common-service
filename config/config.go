6167abe9f31902c14d4f82564b81f58f5eabd431
JonathanInTheClouds <48375298+JonathanInTheClouds@users.noreply.github.com>
2023-12-03 17:14:54 -0600

    🔧 chore(config.go): add LoadEnvWithDefault function to load environment variables with a default value if not set

--- config/config.go
+++ config/config.go
@@ -24,3 +24,28 @@ func LoadEnv(envName string) (string, error) {
 
 	return env, nil
 }
+
+func LoadEnvWithDefault(envName string, defaultValue string) (string, error) {
+	env := os.Getenv(envName)
+	if env == "" {
+		// Load .env file
+		err := godotenv.Load()
+		if err != nil {
+			// If .env loading fails, use the default value
+			if defaultValue != "" {
+				return defaultValue, nil
+			}
+			return "", err
+		}
+
+		env = os.Getenv(envName)
+		if env == "" {
+			if defaultValue != "" {
+				return defaultValue, nil
+			}
+			return "", errors.New(envName + " environment variable not set")
+		}
+	}
+
+	return env, nil
+}
